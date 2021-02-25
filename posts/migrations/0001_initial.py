b281a962f57db0f0916dce302829ef469e231fcc
Kotsenko Maksim <maksimkcenk93@gmail.com>
2021-02-26 00:28:05 +0300

    first_pytest_complete

--- posts/migrations/0001_initial.py
+++ posts/migrations/0001_initial.py
@@ -6,7 +6,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -20,7 +19,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('text', models.TextField()),
                 ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name='date published')),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='posts', to=settings.AUTH_USER_MODEL)),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='posts',
+                                             to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]

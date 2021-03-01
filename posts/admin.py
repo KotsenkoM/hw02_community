132e24165f25c4c75112e395d39e208f63913771
Kotsenko Maksim <maksimkcenk93@gmail.com>
2021-03-01 22:22:22 +0300

    review 2

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -10,4 +10,12 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "slug")
+    search_fields = ("title",)
+    list_filter = ("pub_date",)
+    empty_value_display = "-пусто-"
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
